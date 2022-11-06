22bdbe57cb4c9ad7fe307b69b2628b5a70fa7912
Zawrat <zawrat.dev@gmail.com>
2022-11-06 23:01:21 +0100

    The unnecessary variable has been removed.

--- src/main/java/com/theodinproject/top_projects/caesarCipher/Encryptor.java
+++ src/main/java/com/theodinproject/top_projects/caesarCipher/Encryptor.java
@@ -9,8 +9,6 @@ public class Encryptor {
 
     }
 
-    static char charFromOriginStringIndex;
-
     static Scanner scanner = new Scanner(System.in);
 
 
@@ -18,34 +16,36 @@ public class Encryptor {
 
         System.out.println("[LOG -> INFO]: Running encryptor... \n");
 
-        int numOfChars = sentence.length();
-
         char[] textToArray = sentence.toCharArray();
 
         System.out.println("[LOG -> INFO]: User text length: " + sentence.length() + "\n");
         System.out.println("[LOG -> INFO]: User shift number: " + shiftNumber + "\n");
 
-        for (int i = 0; i < numOfChars; i++) {
+
+        for (int i = 0; i < sentence.length(); i++) {
 
             System.out.println("[LOG -> INFO]: Iteration: " + i + "\n");
 
-            charFromOriginStringIndex = sentence.charAt(i);
+            System.out.println("[LOG -> INFO]: The char from the sentence : " + sentence.charAt(i) + "\n");
 
-            System.out.println("[LOG -> INFO]: charFromOriginStringIndex = " + charFromOriginStringIndex + "\n");
-
-            textToArray[i] = Shifter.shiftCharacter(charFromOriginStringIndex, shiftNumber);
+            textToArray[i] = Shifter.shiftCharacter(sentence.charAt(i), shiftNumber);
 
         }
+
+
         System.out.println("[LOG -> INFO]: Encrypted text: ");
         System.out.println(textToArray);
         return textToArray;
+
     }
 
     static char[] encrypt(String sentence) {
 
+
         System.out.println("[LOG -> INFO]: Running encryptor without a shift number... \n");
 
         System.out.println("Please provide a shift number: ");
+
 
         try {
 
